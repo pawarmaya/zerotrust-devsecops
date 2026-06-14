554667c4a96f326589a16236686a0151aaac155f
pawarmaya <mayapawar2711@gmail.com>
2026-06-14 07:31:55 +0000

    App5

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+import subprocess
 
+def execute_command(user_input):
+    subprocess.call(user_input, shell=True)
 @app.route("/")
 def home():
     return render_template("index.html")
